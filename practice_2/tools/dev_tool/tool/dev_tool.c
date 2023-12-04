eda89e663569e47e871981117a1f6d2188f4489c
AZhaoTongXue <2223757448@qq.com>
2023-12-04 22:16:36 +0800

    完成代码及makefile编写

---
+++ practice_2/tools/dev_tool/tool/dev_tool.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "music.h"
+#include "network.h"
+#include "settings.h"
+#include "oled_debug.h"
+
+int main()
+{
+    // powerup and init
+    printf("*************** powerup and init ***************\n");
+
+    // oled function
+    printf(">>>>>>>>>> oled function test <<<<<<<<<<\n");
+    oled_test();
+
+    // music function
+    printf(">>>>>>>>>> music function test <<<<<<<<<<\n");
+    music_option(MUSIC_START);
+    music_option(MUSIC_VOLUME_UP);
+    music_option(MUSIC_VOLUME_DOWN);
+    music_option(MUSIC_FRONT);
+    music_option(MUSIC_NEXT);
+    music_option(MUSIC_STOP);
+    music_option(MUSIC_RESTART);
+
+    // network function
+    printf("\n>>>>>>>>>> network function test <<<<<<<<<<\n");
+    network_on();
+    network_off();
+
+    // settings function
+    printf("\n>>>>>>>>>> settings function test <<<<<<<<<<\n");
+    set_voice_bluetooth();
+    set_voice_speaker();
+
+    // end
+    printf("********************* end *********************\n");
+
+    return 0;
+}
